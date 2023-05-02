751694e960eeb04b710536d77cadf710951a2ff3
Yana Reinn <amaruelleof@gmail.com>
2023-05-02 19:05:18 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import numpy as np
+from prettytable import PrettyTable
+import sys
+
+import numerical
+
+
+def input_matrix(rows, cols):
+    matrix = []
+    for i in range(rows):
+        row = list(map(float, input(f"Введите строку {i + 1} (разделяйте значения пробелом): ").split()))
+        if len(row) != cols:
+            print("Неправильный набор значений. Выход.")
+            sys.exit(1)
+        matrix.append(row)
+    return matrix
+
+
+def input_vector(length):
+    vector = list(map(float, input("Введите вектор (разделяйте значения пробелом): ").split()))
+    if len(vector) != length:
+        print("Неправильный набор значений. Выход.")
+        sys.exit(1)
+    return vector
+
+
+def main():
+    rows = int(input("Введите количество строк: "))
+    cols = int(input("Введите количество столбцов: "))
+    approximation = [0, 0, 0]
+    max_steps = 20
+    tolerance = 1e-6
+
+    print("Введите матрицу:")
+    matrix = input_matrix(rows, cols)
+
+    print("Введите вектор:")
+    vector = input_vector(rows)
+
+    table = PrettyTable()
+    for i in range(rows):
+        table.add_row(matrix[i])
+
+    print("Введенная матрица:")
+    print(table)
+
+    table = PrettyTable(["Вектор"])
+    for value in vector:
+        table.add_row([value])
+
+    print("Введенный вектор:")
+    print(table)
+
+    result = numerical.seidel(matrix, vector, approximation, max_steps, tolerance)
+    result2 = numerical.jacobi(matrix, vector, approximation, max_steps, tolerance)
+    print("Решение методом Зейделя:", result)
+    print("Решение методом Итераций:", result2)
+
+
+if __name__ == "__main__":
+    main()
---
+++ numerical.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+
+def seidel(matrix, vector, approximation, max_steps, tolerance):
+    print("Решение методом Зейделя:")
+    n = len(matrix)
+    x = approximation
+    step = 1
+
+    for k in range(max_steps):
+        x_old = x.copy()
+
+        for i in range(n):
+            sum1 = sum(matrix[i][j] * x[j] for j in range(i))
+            sum2 = sum(matrix[i][j] * x_old[j] for j in range(i + 1, n))
+            x[i] = (vector[i] - sum1 - sum2) / matrix[i][i]
+
+        print(f"Итерация {step}: {x}")
+        step += 1
+
+        if all(abs(x[i] - x_old[i]) < tolerance for i in range(n)):
+            return x
+
+    print("В заданное количество шагов метод не сошелся, возвращаю последнюю итерацию.")
+    return x
+
+
+def jacobi(matrix, vector, approximation, max_steps, tolerance):
+    print("Решение методом Итераций:")
+    n = len(matrix)
+    x = approximation
+    step = 1
+
+    for k in range(max_steps):
+        x_old = x.copy()
+
+        for i in range(n):
+            sum1 = sum(matrix[i][j] * x_old[j] for j in range(i))
+            sum2 = sum(matrix[i][j] * x_old[j] for j in range(i + 1, n))
+            x[i] = (vector[i] - sum1 - sum2) / matrix[i][i]
+
+        print(f"Итерация {step}: {x}")
+        step += 1
+
+        if all(abs(x[i] - x_old[i]) < tolerance for i in range(n)):
+            return x
+
+    print("В заданное количество шагов метод не сошелся, возвращаю последнюю итерацию.")
+    return x
